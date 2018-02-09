4d0e049f18326d595e7ee04ba81a8de505335fcc
ricco1337 <ricco1337@outlook.com>
2018-02-08 17:35:03 -0800

    first push

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import _ from 'lodash';
+import React, {
+  Component
+} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail'
+
+const API_KEY = 'AIzaSyA_qDBZ4FosDk3D_BMUUJg_L3_nnbYgcns';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('corgi');
+  }
+
+  videoSearch(term){
+    YTSearch({
+      key: API_KEY,
+      term: term
+    },
+    (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+
+       });
+    });
+  }
+
+  render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      < div >
+      <SearchBar onSearchTermChange={videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+       onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+       videos={this.state.videos} />
+
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render( < App / > , document.querySelector('.container'));
